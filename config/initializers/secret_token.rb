2c0f6f97f042e9c7015f2cb5dccabb752dc25726
jamauro <jamauro@gmail.com>
2013-04-01 14:24:08 -0500

    Created a rails app and added initial product creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleshirts::Application.config.secret_token = '547e19067662efc43fd0e7959c72bcf1e9ec75033fd34f5ff36f4994016a67b1905c8bd4b87e108a5ba973f8915fb9991034cbde2324fc9936e66e044f461539'

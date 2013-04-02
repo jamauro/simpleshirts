bf00c58a1872560e986e9e36a858beccbe0ceac3
jamauro <jamauro@gmail.com>
2013-04-01 19:37:37 -0500

    Set up homepage and incorporated bootstrap. Also setup preliminary routing and removed public index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Simpleshirts::Application.routes.draw do
 
+  get "home/index"
+
+  root :to => 'home#index'
+  
   resources :products
 
   resources :charges
@@ -52,7 +56,7 @@ Simpleshirts::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 

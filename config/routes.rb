be6976c2d53cf510bf1f70570d0c0d9120bf3add
jamauro <jamauro@gmail.com>
2013-04-02 18:33:05 -0500

    Install devise to handle user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Simpleshirts::Application.routes.draw do
 
+  devise_for :users
+
   get "home/index"
 
   root :to => 'home#index'
@@ -56,7 +58,7 @@ Simpleshirts::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 

b16b059cc0cb41ba86ff0ca7d7afcb0dc3d9bb3f
jamauro <jamauro@gmail.com>
2013-04-02 19:07:22 -0500

    Added support for admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Simpleshirts::Application.routes.draw do
 
+  devise_for :admins
+
   devise_for :users
 
   get "home/index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130402232802) do
+ActiveRecord::Schema.define(:version => 20130402235146) do
+
+  create_table "admins", :force => true do |t|
+    t.string   "username"
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
   create_table "products", :force => true do |t|
     t.string   "name"
